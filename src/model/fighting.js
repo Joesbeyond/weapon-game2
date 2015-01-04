2c923c7234472bff46fee4a6cf84677b5e561c62
joesbeyond <joesbeyond@gmail.com>
2015-01-04 16:26:15 +0800

    add delayTime of poison and fire

--- src/model/fighting.js
+++ src/model/fighting.js
@@ -25,26 +25,48 @@ Fighting.prototype.fight = function () {
             break;
         }
 
-        if(this.soldier.weapon.Effect.name === '毒性' && this.soldier.weapon.Effect.name === '火焰') {
+        if(this.soldier.weapon.Effect.name === '毒性' || this.soldier.weapon.Effect.name === '火焰') {
 
         this.soldier.hp -= ordinaryAttack;
-        this.ordinary.hp -= soldierAttack;    
+        this.ordinary.hp -= soldierAttack;  
+        delayTimes-- ; 
+       // console.log(); 
         var odd = _.random(0, 100);
+        var excuteTimes = 0;
         if (odd < this.soldier.weapon.Effect.odds) {
             result += this.soldier.profession + this.soldier.name + '用' + this.soldier.weapon.name +
-            '攻击了' + this.ordinary.profession + this.ordinary.name + ',' + this.ordinary.name + '受到了' +
-            soldierAttack + '点伤害,' + this.ordinary.name + this.soldier.weapon.Effect.behaviour +'了,' + this.ordinary.name + '剩余生命：'+
-            this.ordinary.hp + '\n' +
-            this.ordinary.name + '受到' + this.soldier.weapon.Effect.attack +
-            '点'+this.soldier.weapon.Effect.name+'伤害,' + this.ordinary.name + '剩余生命：' + (this.ordinary.hp - this.soldier.weapon.Effect.attack) +'\n';
+                    '攻击了' + this.ordinary.profession + this.ordinary.name + ',' + this.ordinary.name + '受到了' +
+                    soldierAttack + '点伤害,' + this.ordinary.name + this.soldier.weapon.Effect.behaviour +'了,' + this.ordinary.name + '剩余生命：'+
+                    this.ordinary.hp + '\n' +
+                    this.ordinary.name + '受到' + this.soldier.weapon.Effect.attack +
+                    '点'+this.soldier.weapon.Effect.name+'伤害,' + this.ordinary.name + '剩余生命：' + (this.ordinary.hp - this.soldier.weapon.Effect.attack) +'\n';
+                    var delayTimes = this.soldier.weapon.Effect.delay;
+                    excuteTimes++ ;
+                    console.log(excuteTimes++ );
+                   // console.log(delayTimes);
+        }else {
+            result += this.soldier.name + '进攻\n';
+            if(delayTimes === 1){
+                //李四受到2点毒性伤害, 李四剩余生命：x
+               // console.log(this.ordinary.hp);
+                result += this.ordinary.name + '受到' + this.soldier.weapon.Effect.attack +'点'+this.soldier.weapon.Effect.name+'伤害,' + this.ordinary.name + '剩余生命：' + (this.ordinary.hp - this.soldier.weapon.Effect.attack) +'\n';
+            
+            }
+
         }
             this.ordinary.hp = this.ordinary.hp - this.soldier.weapon.Effect.attack;
 
+            if (this.ordinary.hp <= 0) {
+            result += this.soldier.profession + this.soldier.name + '使用最后一击，把' + this.ordinary.name + '打死了';
+            break;
+            }
             result += this.ordinary.profession + this.ordinary.name + '攻击了' + this.soldier.profession + this.soldier.name +
                     ',攻击了' + ordinaryAttack + '点伤害,' + this.soldier.name + '的' + this.soldier.armor.name +
                     '抵挡了' + this.soldier.armor.defense + '点伤害' + this.soldier.name + '还剩' + this.soldier.hp + '点血' + '\n';
-            
+
+
         };
+            
 
         if(this.soldier.weapon.Effect.name === '冰冻') {
 
@@ -70,11 +92,17 @@ Fighting.prototype.fight = function () {
 
             this.ordinary.hp -= soldierAttack; 
             this.soldier.weapon.Effect.delay -= 1;
+            var odd = _.random(0, 100);
+          //  var swimTimes = 0;
+            //if (odd < this.soldier.weapon.Effect.odds) {
             result += this.soldier.profession + this.soldier.name + '用' + this.soldier.weapon.name +
                     '攻击了' + this.ordinary.profession + this.ordinary.name + ',' + this.ordinary.name + '受到了' +
                     soldierAttack + '点伤害,' + this.ordinary.name + this.soldier.weapon.Effect.behaviour +'了,' + this.ordinary.name + '剩余生命：'+
                     this.ordinary.hp + '\n'
-          
+               //     swimTimes++ ;
+                //    console.log('0000');
+          //      }; 
+           // this.soldier.weapon.Effect.delay += swimTimes;
             if(this.soldier.weapon.Effect.delay >= 0) {
                 result += this.ordinary.name + '晕倒了，无法攻击,' + this.soldier.weapon.Effect.name + '还剩：' + this.soldier.weapon.Effect.delay + '轮\n';
             }else {
@@ -89,6 +117,8 @@ Fighting.prototype.fight = function () {
             
             } 
         };
+
+
         if(this.soldier.weapon.Effect.name === '利剑') {
             result += this.soldier.profession + this.soldier.name + '用' + this.soldier.weapon.name +
                     '攻击了' + this.ordinary.profession + this.ordinary.name + ',' + this.soldier.name + '发动了'+
